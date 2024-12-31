60dfbd98ef9632004c3aff4a7424367d074fd217
maxiimize <max.b@hotmail.se>
2024-12-31 21:44:41 +0100

    Added interface for services

---
+++ HotellGK/Services/IServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotellGK.Services
+{
+    public interface IService<T>
+    {
+        void Add(T entity);
+        List<T> GetAll();
+        void Update(int id, T entity);
+        void Delete(int id);
+    }
+}
