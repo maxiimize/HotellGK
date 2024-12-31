1a5f20d3a95b2d589e70a40b63ee8dd1500a8e93
maxiimize <max.b@hotmail.se>
2024-12-31 21:59:55 +0100

    Created RoomService class and wrote method for adding a room

---
+++ HotellGK/Services/RoomService.cs
@@ -0,0 +1,26 @@
+﻿using HotellGK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotellGK.Services
+{
+    internal class RoomService : IService<Room>
+    {
+        private readonly HotelDbContext Context;
+
+        public RoomService(HotelDbContext context)
+        {
+            Context = context;
+        }
+
+        public void Add(Room entity)
+        {
+            Context.Rooms.Add(entity);
+            Context.SaveChanges();
+            Console.WriteLine("Room added successfully!");
+        }
+    }
+}
