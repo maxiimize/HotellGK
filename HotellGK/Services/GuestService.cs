f84b55b6308a30d8704903df1b52c967e070fef7
maxiimize <max.b@hotmail.se>
2025-01-01 22:43:16 +0100

    Created GuestService and added Add method

---
+++ HotellGK/Services/GuestService.cs
@@ -0,0 +1,26 @@
+﻿using HotellGK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotellGK.Services
+{
+    public class GuestService
+    {
+        private readonly HotelDbContext _context;
+
+        public GuestService(HotelDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Guest entity)
+        {
+            _context.Guests.Add(entity);
+            _context.SaveChanges();
+            Console.WriteLine("Guest added successfully!");
+        }
+    }
+}
